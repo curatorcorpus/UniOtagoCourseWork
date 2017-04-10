7dc402e74a9a73171c5167713c28656f4047f744
curatorcorpus <itachi383@hotmail.com>
2017-04-10 20:17:41 +1200

    initial commit

---
+++ RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/Skeleton/renderApp.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/Skeleton/renderApp.cpp.o"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/common/Camera.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/common/Camera.cpp.o"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/common/Controls.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/common/Controls.cpp.o"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/common/Group.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/common/Group.cpp.o"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/common/MTLShader.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/common/MTLShader.cpp.o"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/common/Material.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/common/Material.cpp.o"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/common/Mesh.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/common/Mesh.cpp.o"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/common/Object.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/common/Object.cpp.o"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/common/Objloader.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/common/Objloader.cpp.o"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/common/Scene.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/common/Scene.cpp.o"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/common/Shader.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/common/Shader.cpp.o"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/common/Texture.cpp" "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/CMakeFiles/Skeleton.dir/common/Texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "USE_ASSIMP"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/glfw-3.1.2/include/GLFW"
+  "../external/glm-0.9.7.1"
+  "../external/glew-1.13.0/include"
+  "../external/assimp-3.0.1270/include"
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/external/glfw-3.1.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/external/CMakeFiles/GLEW_1130.dir/DependInfo.cmake"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/external/assimp-3.0.1270/code/CMakeFiles/assimp.dir/DependInfo.cmake"
+  "/home/curator/Assignment2OptionB/RenderEngineSkeletonCode/build/external/assimp-3.0.1270/contrib/zlib/CMakeFiles/zlib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
