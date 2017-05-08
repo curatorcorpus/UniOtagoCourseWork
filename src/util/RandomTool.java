a8ad95e8fa414ed5b4280645db40531010d14fcb
curatorcorpus <itachi383@hotmail.com>
2017-05-08 20:51:28 +1200

    seeding implemented

--- src/util/RandomTool.java
+++ src/util/RandomTool.java
@@ -12,6 +12,6 @@ import java.util.Random;
  * @author curator
  */
 public class RandomTool {
-    
+    private static final long SEED = 10;
     public static final Random random = new Random();
 }
