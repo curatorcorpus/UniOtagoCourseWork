ecb1956b74e0969ebcb8fa33108fc254a596836f
curatorcorpus <itachi383@hotmail.com>
2017-07-26 17:51:30 +1200

    first commit

---
+++ Assets/Scripts/OctreeController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OctreeController : MonoBehaviour {
+
+	private OctreeNode<int> root;
+
+	public float size  = 5.0f;
+	public int   depth = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+createOctree();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void createOctree() 
+	{
+		root = new OctreeNode<int>(this.transform.position, size);
+		root.subdivide(depth);
+	}    
+
+	void OnDrawGizmos()
+    {
+        //createOctree();
+
+        //DrawNode(root);
+    }
+
+    private Color minColor = new Color(1, 1, 1, 1f);
+    private Color maxColor = new Color(0, 0.5f, 1, 0.25f);
+
+    private void DrawNode(OctreeNode<int> node, int nodeDepth = 0)
+    {
+        if (!node.isLeaf())
+        {
+            foreach (var child in node.Children)
+            {
+                DrawNode(child, nodeDepth + 1);
+            }
+        }
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(node.Position, Vector3.one * node.Size);
+    }
+}
---
+++ Assets/Scripts/OctreeNode.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctreeNode<TType> {
+
+	public enum OctreeIndex
+	{
+	    BottomLeftFront = 0, //000,
+	    BottomRightFront = 2, //010,
+	    BottomRightBack = 3, //011,
+	    BottomLeftBack = 1, //001,
+	    TopLeftFront = 4, //100,
+	    TopRightFront = 6, //110,
+	    TopRightBack = 7, //111,
+	    TopLeftBack = 5, //101,
+	}
+
+	private static int DEGREE = 8;
+
+	private float size;
+
+	private Vector3 position;
+
+	private OctreeNode<TType>[] children;
+
+	public OctreeNode(Vector3 position, float size) 
+	{
+		this.position = position;
+		this.size 	  = size;
+	}
+
+	public Vector3 Position
+	{
+		get { return position; }
+		set { this.position = value; }
+	}
+
+	public float Size
+	{
+		get { return size; }
+		set { this.size = value; }
+	}
+
+	public bool isLeaf()
+	{
+		return children == null;
+	}
+
+	public void subdivide(int depth = 0)
+	{
+		children = new OctreeNode<TType>[DEGREE];
+		for (int i = 0; i < children.Length; i++)
+        {
+            Vector3 newPos = position;
+            if ((i & 4) == 4) newPos.y += size * 0.25f;
+            else 			  newPos.y -= size * 0.25f;
+
+            if ((i & 2) == 2) newPos.x += size * 0.25f;
+            else 			  newPos.x -= size * 0.25f;
+
+            if ((i & 1) == 1) newPos.z += size * 0.25f;
+            else 			  newPos.z -= size * 0.25f;
+
+            children[i] = new OctreeNode<TType>(newPos, size * 0.5f);
+            if (depth > 0)
+            {
+                children[i].subdivide(depth - 1);
+            }
+        }
+	}
+
+	// allows foreach children
+	public IEnumerable<OctreeNode<TType>> Children
+	{
+		get {return children; }
+	}
+}
