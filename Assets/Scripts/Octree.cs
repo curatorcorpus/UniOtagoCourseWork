18bde75f26306aac51ae1bd3ad9034db3bfaff7d
curatorcorpus <itachi383@hotmail.com>
2017-07-30 11:06:36 +1200

    bug needs fixing when drawing game object cubes to represent voxels

--- Assets/Scripts/Octree.cs
+++ Assets/Scripts/Octree.cs
@@ -6,7 +6,18 @@ public class Octree<TType>
 {
     private OctreeNode<TType> root;
 
+    private int count;
     private int depth = 0;
+    private float maxSize;
+    private float maxPoint;
+
+    // GETTERS AND SETTERS
+
+    public int Count
+    {
+        get { return count; }
+        //set { this.count = value;  }
+    }
 
     public OctreeNode<TType> Root
     {
@@ -18,12 +29,41 @@ public class Octree<TType>
     {
         this.depth = depth;
         this.root  = new OctreeNode<TType>(position, size);
+        this.maxSize  = size;
+        this.maxPoint = maxSize / 2;
+        this.count = 0;
     }
 
     // PUBLIC METHODS
     public void add(Vector3 pos)
     {
-        root.add(pos, depth);
+        if (pos.x <= maxPoint && pos.x >= -maxPoint &&
+            pos.y <= maxPoint && pos.y >= -maxPoint &&
+            pos.z <= maxPoint && pos.z >= -maxPoint)
+        {
+            root.add(pos, depth);
+
+            count++;
+        }
+
+        // any position outside mentioned boundary is just inserted at the boundary points.
+        else
+        {
+            Debug.Log("position " + pos + " wasn't inserted because the max size is " + maxSize);
+            Debug.Log("The maximum boundaries are: ");
+            printBoundaries();
+        }
     }
 
+    public void printBoundaries()
+    {
+        Debug.Log(new Vector3(maxPoint, maxPoint, maxPoint));
+        Debug.Log(new Vector3(-maxPoint, maxPoint, maxPoint));
+        Debug.Log(new Vector3(maxPoint, -maxPoint, maxPoint));
+        Debug.Log(new Vector3(maxPoint, maxPoint, -maxPoint));
+        Debug.Log(new Vector3(-maxPoint, -maxPoint, maxPoint));
+        Debug.Log(new Vector3(-maxPoint, maxPoint, -maxPoint));
+        Debug.Log(new Vector3(maxPoint, -maxPoint, -maxPoint));
+        Debug.Log(new Vector3(-maxPoint, -maxPoint, -maxPoint));
+    }
 }
--- Assets/Scripts/OctreeController.cs
+++ Assets/Scripts/OctreeController.cs
@@ -1,19 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class OctreeController : MonoBehaviour {
 
     private bool drawTree = false;
-
     private Octree<int> tree;
+    private List<GameObject> voxels;
 
 	public float size  = 5.0f;
 	public int   depth = 2;
+
+    // used for debugging
+    void OnDrawGizmos()
+    {
+        if (drawTree)
+        {
+            gizmosDrawNode(tree.Root);
+        }
+    }
 
     // Use this for initialization
     void Start ()
     {
         tree = new Octree<int>(this.transform.position, size, depth);
+
         tree.add(new Vector3(0.25f, 0.25f, 0.25f));
         tree.add(new Vector3(-0.25f, 0.25f, 0.25f));
         tree.add(new Vector3(0.25f, -0.25f, 0.25f));
@@ -23,23 +34,59 @@ public class OctreeController : MonoBehaviour {
         tree.add(new Vector3(-0.25f, 0.25f, -0.25f));
         tree.add(new Vector3(-2f, -2f, -2f));
 
+        tree.add(new Vector3(5f, 5f, 5f));
+        tree.add(new Vector3(-5f, -5f, -5f));
+        tree.add(new Vector3(5f, -5f, 5f));
+        tree.add(new Vector3(5f, 5f, -5f));
+        tree.add(new Vector3(-5f, 5f, 5f));
+        tree.add(new Vector3(-5f, 5f, -5f));
+        tree.add(new Vector3(-5f, -5f, 5f));
+        tree.add(new Vector3(5f, -5f, -5f));
+
+        tree.add(new Vector3(20f, 20f, 20f));
+
+        voxels = new List<GameObject>(tree.Count);
+
+        for(int i = 0; i < tree.Count; i++)
+        {
+            GameObject voxel = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+
+            voxels.Add(voxel);
+        }
+
         drawTree = true;
     }
 
     // Update is called once per frame
-    void Update() {} 
-
-	void OnDrawGizmos()
+    /*void Update()
     {
-        if(drawTree)
-        {
-            DrawNode(tree.Root);
-        }
-    }
+        voxelDrawNode(tree.Root, depth);
+    } */
 
     // PRIVATE METHODS
+    private void voxelDrawNode(OctreeNode<int> node, int nodeDepth = 0)
+    {
+        if (node == null)
+        {
+            return;
+        }
 
-    private void DrawNode(OctreeNode<int> node, int nodeDepth = 0)
+        if (!node.isLeaf())
+        {
+            foreach (var child in node.Children)
+            {
+                voxelDrawNode(child, nodeDepth + 1);
+            }
+        }
+
+        // draw
+        GameObject voxel = voxels[0];
+
+        voxel.transform.position = node.Position;
+    }
+
+    private void gizmosDrawNode(OctreeNode<int> node, int nodeDepth = 0)
     {
         if(node == null)
         {
@@ -50,7 +97,7 @@ public class OctreeController : MonoBehaviour {
         {
             foreach (var child in node.Children)
             {
-                DrawNode(child, nodeDepth + 1);
+                gizmosDrawNode(child, nodeDepth + 1);
             }
         }
         Gizmos.color = Color.green;
