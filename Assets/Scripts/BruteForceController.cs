c366a8db634d3fd1d45e0e98eb5100c0924c1780
curatorcorpus <itachi383@hotmail.com>
2017-07-30 13:53:00 +1200

    brute force data structures added

---
+++ Assets/Scripts/BruteForceController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BruteForceController : MonoBehaviour {
+
+    private int[, ,] voxelspace;
+
+    public int maxLength = 3;
+
+    public int voxelLength = 25; // x
+    public int voxelHeight = 25; // y
+    public int voxelDepth = 25; // z
+
+    private float voxelSize;
+    private float voxelSizeHalf;
+
+    // Use this for initialization
+    void Start ()
+    {
+        voxelspace = new int[voxelLength, voxelHeight, voxelDepth];
+        voxelSize = ((float)maxLength / (float)voxelLength);
+        voxelSizeHalf = voxelSize / 2;
+
+        for (int x = 1; x <= voxelLength; x++)
+        {
+            for (int y = 1; y <= voxelHeight; y++)
+            {
+                for (int z = 1; z <= voxelDepth; z++)
+                {
+                    GameObject voxel = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    MeshRenderer shader = voxel.gameObject.GetComponent<MeshRenderer>();
+
+                    shader.material.color = Random.ColorHSV();
+                    voxel.SetActive(false);
+
+                    Vector3 newPos = new Vector3();
+
+                    newPos.x = x * voxelSize;
+                    newPos.y = y * voxelSize;
+                    newPos.z = z * voxelSize;
+
+                    voxel.transform.localPosition = newPos;
+                    voxel.transform.localScale = new Vector3(voxelSize, voxelSize, voxelSize);
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Voxel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voxel {
+
+    //private char* label;
+    private Color32 colour;
+    
+    public Voxel(string lab, Color32 colour)
+    {
+        //fixed (char* label = lab) { }
+        //this.label = label;
+        this.colour = colour;  
+    }
+
+    public Color32 Colour
+    {
+        get { return colour; }
+        set { colour = value; }
+    }
+}
