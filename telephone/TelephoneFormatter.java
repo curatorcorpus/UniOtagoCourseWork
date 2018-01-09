48443176cf6db9b25ec4c4a938b490c7bb3de00c
curatorcorpus <itachi383@hotmail.com>
2018-01-09 22:51:43 +1300

    telephone: functionality for testing type of phone number by prefix added

--- telephone/TelephoneFormatter.java
+++ telephone/TelephoneFormatter.java
@@ -4,13 +4,16 @@ public class TelephoneFormatter {
     // ENUM TYPES.
     
     private enum Prefixes {
-	FREEPHONE, MOBILE, LANDLINE, INVALID
+	FREEPHONE, MOBILE, UNUSED_MOBILE, LANDLINE, INVALID
     }
     
 
     // DATA FIELDS.
     
-    private String freephonePrefixRegx = "(0508|0800|09000)";
+    private String freephoneRegx    = "(0508|0800|09000)";
+    private String mobileRegx       = "(021|022|027)";
+    private String unusedMobileRegx = "(025)";
+    private String landlineRegx     = "(02|03|04|05|06|07|09)";
     
     // CONSTRUCTORS.
     
@@ -52,6 +55,22 @@ public class TelephoneFormatter {
 
 	// determine if phone number is a FREEPHONE.
 	String prefix = number.substring(0,4);
+
+	if(prefix.matches(freephoneRegx)) {
+	    return Prefixes.FREEPHONE;
+	}
+
+	prefix = prefix.substring(0,3);
+	if(prefix.matches(mobileRegx)) {
+	    return Prefixes.MOBILE;
+	} else if(prefix.matches(unusedMobileRegx)) {
+	    return Prefixes.UNUSED_MOBILE;
+	}
+
+	prefix = prefix.substring(0,2);
+	if(prefix.matches(landlineRegx)) {
+	    return Prefixes.LANDLINE;
+	}
 	
 	return Prefixes.INVALID;
     }
