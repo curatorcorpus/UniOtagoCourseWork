8eb775ed8e0e19e7c877954ad1c37d834fd763cb
curatorcorpus <itachi383@hotmail.com>
2018-01-09 21:49:21 +1300

    telephone: basic testdata and basic file reading added

---
+++ telephone/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+	String filename = "";
+
+	// determine we have a filename by args.
+	try {
+
+	    filename = args[0] + ".txt";	    
+        } catch(ArrayIndexOutOfBoundsException e) {
+	    System.err.println("Input file not provided by argument!");
+	    return;
+	}
+
+
+	// read each line of each unformatted phone number.
+	try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
+
+	    StringBuilder sb = new StringBuilder();
+
+	    String line = "";
+	    while((line = br.readLine()) != null) {
+		System.out.println(line);
+	    }
+
+	} catch(FileNotFoundException e) {
+	    System.err.println("File not found!");
+	} catch(IOException e) {
+	    System.err.println("Incorrect Input");
+	}
+    }
+}
