087a1287a5116786084d593d902a76b18f9b973a
curatorcorpus <itachi383@hotmail.com>
2018-03-14 21:36:19 +1300

    plane to point distance method fixed

--- planeFinder.cpp
+++ planeFinder.cpp
@@ -40,14 +40,14 @@ int main (int argc, char *argv[]) {
     std::cout << "Recolouring points" << std::endl;
     std::vector<Vector3i> colours;
 
-    vector<PlyPoint>* points = ply.get_points();
+    vector<PlyPoint>* point_cloud = ply.get_points();
     /*
     for(PlyPoint point : *points) {
         cout << point.location << endl;
     }*/
-
+Ransac::search(point_cloud, 2, 0.05, 2);
     //Vector4d plane = Plane::get_plane((*points)[0].location, (*points)[1].location, (*points)[2].location);
-    Vector4d plane = Plane::compute_plane(Vector3d(0,0,0), Vector3d(1,2,3), Vector3d(3,2,1));
+    //Vector4d plane = Plane::compute_plane(Vector3d(0,0,0), Vector3d(1,2,3), Vector3d(3,2,1));
     /*cout << plane << endl << endl;
     cout << (*points)[0].location << endl << endl;
     double dist = Ransac::distance_to_plane(plane, (*points)[0].location);
@@ -59,16 +59,16 @@ cout << dist << endl << endl;*/
     /*
     size_t planeSize = ply.size()/nPlanes;
     for (size_t ix = 0; ix < ply.size(); ++ix) {
-    size_t planeIx = ix / planeSize;
-    size_t colourIx = planeIx % colours.size(); // May need to recycle colours
-    ply[ix].colour = colours[colourIx];
+        size_t planeIx = ix / planeSize;
+        size_t colourIx = planeIx % colours.size(); // May need to recycle colours
+        ply[ix].colour = colours[colourIx];
     }
     *//*
     // Write the resulting (re-coloured) point cloud to a PLY file.
     std::cout << "Writing PLY data to " << argv[2] << std::endl;
     if (!ply.write(argv[2])) {
-    std::cout << "Could not write PLY data to file " << argv[2] << std::endl;
-    return -2;
+        std::cout << "Could not write PLY data to file " << argv[2] << std::endl;
+        return -2;
     }*/
 
     return 0;
--- ransac.cpp
+++ ransac.cpp
@@ -22,7 +22,7 @@ double Ransac::distance_to_plane(Vector4d plane, Vector3d point)
     normal[1] = plane[1];
     normal[2] = plane[2];
 
-    double nominator   = plane.dot(_point);
+    double nominator   = std::abs(plane.dot(_point));
     double denominator = std::sqrt(normal.dot(normal));
 
     return nominator / denominator;
@@ -33,20 +33,49 @@ double Ransac::distance_to_plane(Vector4d plane, Vector3d point)
 */
 void Ransac::search(std::vector<PlyPoint>* point_cloud, int no_planes, double threshold_distance, int no_ransac_trials)
 {   
-    int pc_size = (*point_cloud).size();
+    // make deep copy
+    std::vector<PlyPoint> pc_cpy = (*point_cloud);
 
     // for each plane count until max number of planes.
     for(int p = 0; p < no_planes; p++) 
     {
+        int pc_size = pc_cpy.size();
+
+        Vector4d best_plane;
+        std::vector<int> best_points;
+
         // for each ransac trial until max number of ransac trials.
-        for(int r = 1; r < no_ransac_trials; r++)
+        for(int r = 0; r < no_ransac_trials; r++)
         {
             // generate plane from three random points
-            Vector4d plane = Plane::compute_plane((*point_cloud)[rand()%pc_size].location,
-                                              (*point_cloud)[rand()%pc_size].location, 
-                                              (*point_cloud)[rand()%pc_size].location);
+            Vector4d plane = Plane::compute_plane(pc_cpy[rand()%pc_size].location,
+                                                  pc_cpy[rand()%pc_size].location, 
+                                                  pc_cpy[rand()%pc_size].location);
+            std::vector<int> curr_pc;
+            // for each point in the point cloud.
+            for(int i = 0; i < pc_size; i++) 
+            {
+                Vector3d point = pc_cpy[i].location;
 
+                // if point distance to plane is less than threshold distance.
+                if(distance_to_plane(plane, point) < threshold_distance) 
+                {
+                    curr_pc.push_back(i);
+                }
+            }
 
+            if(curr_pc.size() > best_points.size()) 
+            {
+                best_plane  = plane;
+                best_points = curr_pc;
+            }
+        }
+        std::cout << best_points.size() << std::endl;
+std::cout << "TEST" << std::endl;
+        // remove best pc from point cloud copy.
+        for(int i = 0; i < best_points.size(); i++) 
+        {std::cout << pc_cpy.capacity() << std::endl; 
+            pc_cpy.erase(pc_cpy.begin() +best_points[i]);
         }
     }
 }
